5e1dad292dc5622389ae11d20c7dfd51371a4f0f
MyKhant <myokhantkyaw148@gmail.com>
2023-01-11 10:24:24 +0630

    MainController

--- src/main/java/com/example/corsdemo/controller/MainController.java
+++ src/main/java/com/example/corsdemo/controller/MainController.java
@@ -2,7 +2,10 @@ package com.example.corsdemo.controller;
 
 import org.springframework.stereotype.Controller;
 
+import java.util.logging.Logger;
+
 @Controller
 public class MainController {
-
+    private Logger logger =
+            Logger.getLogger(MainController.class.getName());
 }
