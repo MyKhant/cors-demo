b5ee7c5f5532d47c9adb27b2d407101ed45d331b
MyKhant <myokhantkyaw148@gmail.com>
2023-01-11 10:53:35 +0630

    ProjectConfig

--- src/main/java/com/example/corsdemo/controller/MainController.java
+++ src/main/java/com/example/corsdemo/controller/MainController.java
@@ -1,6 +1,10 @@
 package com.example.corsdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.logging.Logger;
 
@@ -8,4 +12,17 @@ import java.util.logging.Logger;
 public class MainController {
     private Logger logger =
             Logger.getLogger(MainController.class.getName());
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+
+    @PostMapping("/test")
+    @ResponseBody
+    @CrossOrigin("http://localhost:8080")
+    public String test(){
+        logger.info("Test method called");
+        return "Hello";
+    }
 }
